7d3444362d2d1f3a1906ad8bbc04f29d2dfba1bc
Samir Ramic <ramic.samirr@gmail.com>
2025-11-04 13:27:17 +0100

    - Details with link

--- Apex Predators/Detail/PredatorDetailView.swift
+++ Apex Predators/Detail/PredatorDetailView.swift
@@ -31,18 +31,45 @@ struct PredatorDetailView: View {
                         .shadow(color: .black, radius: 7)
                         .offset(y: 20)
                 }
-            
-                // Name
-            
-                // Location
-            
-                // Appears in
-            
-                // Movie scenes
-            
-                // Link
+                
+                Spacer().frame(height: 50)
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    // Name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    
+                    // Location
+                    
+                    // Appears in movies
+                    VStack(alignment: .leading){
+                        Text("Appears in:").font(.title2)
+                        Spacer()
+                        ForEach(predator.movies, id: \.self){ movie in
+                            Text("\u{2022} \(movie)")
+                        }
+                    }
+                  
+                    // Movie scenes
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Movie Moments").font(.title)
+                  
+                        ForEach(predator.movieScenes){ movieScene in
+                            Spacer()
+                            Text(movieScene.movie)       .font(.title3)
+                                .fontWeight(.semibold)
+                    
+                            Text(movieScene.sceneDescription)
+                        }
+                    }
+
+                    // Link
+                    Link("About: \(predator.name)",
+                         destination: URL(string: "\(predator.link)")!)
+                    
+                }.padding()
             }
-          
         }
         .ignoresSafeArea()
         .preferredColorScheme(ColorScheme.dark)
@@ -50,6 +77,6 @@ struct PredatorDetailView: View {
 }
 
 #Preview {
-    let predatorFactory = PredatorFactory().allapexPredators.first!
+    let predatorFactory = PredatorFactory().allapexPredators[1]
     PredatorDetailView(predator: predatorFactory)
 }
