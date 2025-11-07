9ef17e92cef4e4d7a7f040b5d0c7cebe5661dedf
Samir Ramic <ramic.samirr@gmail.com>
2025-11-07 08:59:09 +0100

    - map style

--- Apex Predators/Detail/MapDetailView.swift
+++ Apex Predators/Detail/MapDetailView.swift
@@ -11,6 +11,7 @@ struct MapDetailView: View {
   
     let predators: [ApexPredator] = PredatorFactory().allapexPredators
     @State var mapCameraPosition: MapCameraPosition
+    @State var satelite: Bool = false
     
     var body: some View {
         Map(position: $mapCameraPosition){
@@ -18,17 +19,42 @@ struct MapDetailView: View {
                 Annotation(predator.name, coordinate: predator.location){
                     Image(predator.imageUrl)
                         .resizable()
-                        .frame(width: 100, height: 100)
+                        .scaledToFit()
+                        .frame(height: 100)
+                        .shadow(color: .white, radius: 10)
+                        .scaleEffect(x: -1)
                 }.annotationTitles(.hidden)
+            }
+        }
+        .mapStyle(
+            satelite ? 
+                .imagery(elevation: .realistic) : 
+                    .standard(elevation: .realistic)
+        )
+        .overlay(alignment: .bottomTrailing) {
+            Button{
+                satelite.toggle()
+            } label: {
+                Image(systemName: satelite ? "globe.americas.fill" : "globe.americas")
+                    .font(.largeTitle)
+                    .imageScale(.large)
+                    .padding(.trailing, 25)
+                    .background(.ultraThinMaterial)
             }
         }
     }
 }
 
 #Preview {
-    @Previewable @State var mapCameraPosition: MapCameraPosition = .camera(MapCamera(centerCoordinate: PredatorFactory().allapexPredators[1].location, distance: 3000))
-    let predator = PredatorFactory().allapexPredators[1]
+    @Previewable @State var mapCameraPosition: MapCameraPosition = .camera(MapCamera(
+        centerCoordinate: PredatorFactory().allapexPredators[2].location,
+        distance: 1000,
+        heading: 250,
+        pitch: 80))
+    
+    let predator = PredatorFactory().allapexPredators[2]
     let position = predator.location
     
     MapDetailView(mapCameraPosition: mapCameraPosition)
+        .preferredColorScheme(.dark)
 }
